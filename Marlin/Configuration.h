283b3e5f73cbea0ffec16108a916eb1600c116e1
ant3dprintwa <66861841+ant3dprintwa@users.noreply.github.com>
2020-06-21 12:04:30 +0800

    add pid values for hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -792,9 +792,9 @@ Board name: FYSETCV1_1A, FYSETCV1_1B, FYSETCV1_2A, FYSETCV1_2B, change_value = S
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 28.66
+  #define DEFAULT_Ki 2.03
+  #define DEFAULT_Kd 101.09
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
